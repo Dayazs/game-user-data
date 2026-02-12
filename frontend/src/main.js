f857b0ff3fcc92623ea62543945c4d9610192c0b
Dayazs <Dayazs@outlook.com>
2026-02-12 20:37:44 +0800

    2月12新增，页面结构更新

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// 引入全局样式
+import '@/css/global.css'
 import router from './router'
 
 createApp(App)
   .use(router)
   .mount('#app')
+
+
+function setRootFontSize() {
+  const baseWidth = 1920   // 设计稿宽度
+  const minWidth = 1200    // 最小缩放宽度
+
+  const width = Math.max(window.innerWidth, minWidth)
+  const fontSize = (width / baseWidth) * 16
+
+  document.documentElement.style.fontSize = fontSize + 'px'
+}
+
+setRootFontSize()
+window.addEventListener('resize', setRootFontSize)
--- frontend/src/view/Home.vue
+++ frontend/src/view/Home.vue
@@ -1,132 +1,114 @@
 <template>
-  <!-- 角色列表 -->
-  <div class="roles">
-    <div class="role-card" v-for="role in roles" :key="role.id">
-      <!-- 左上角 定位 + 属性 -->
-      <div class="role-icons" v-if="role.rankInfo">
-        <img :src="getRankImg(role.rankInfo[0].role_type)" />
-        <img :src="getDamageImg(role.rankInfo[0].damage_type)" />
+  <!-- 外部最大盒子 -->
+  <div class="outer-box">
+    <!-- 左侧导航区 -->
+    <div class="left-nav">
+      <!-- 返回首页 -->
+      <div class="logo"></div>
+      <!-- 主导航 -->
+      <div class="nav">
+        <div class="nav-item"><a href="">角色</a></div>
+        <div class="nav-item"><a href="">武器</a></div>
+        <div class="nav-item"><a href="">装备</a></div>
       </div>
+    </div>
 
-      <!-- 角色背景图 -->
-      <img class="role-bg" :src="getCharacterImg(role.spell)" />
-
-      <!-- 角色名字 -->
-      <div class="role-name">
-        {{ role.name }}
+    <!-- 右侧内容区 -->
+    <div class="right-content">
+      <!-- 筛选区 -->
+      <div class="filters">
+        <!-- 星级 -->
+        <div class="filter-box">
+          <div class="filter-label">星级</div>
+          <div class="star-options">
+            <div class="star-item">6星</div>
+            <div class="star-item">5星</div>
+            <div class="star-item">4星</div>
+          </div>
+        </div>
+        <!-- 属性 -->
+        <div class="filter-box">
+          <div class="filter-label">属性</div>
+          <div class="icon-items">
+            <div class="icon-item" title="寒冷"></div>
+            <div class="icon-item" title="灼热"></div>
+            <div class="icon-item" title="物理"></div>
+            <div class="icon-item" title="自然"></div>
+            <div class="icon-item" title="电磁"></div>
+          </div>
+        </div>
+        <!-- 类型 -->
+        <div class="filter-box">
+          <div class="filter-label"></div>
+          <div class="icon-items">
+            <div class="icon-item" title="术士"></div>
+            <div class="icon-item" title="突击"></div>
+            <div class="icon-item" title="先锋"></div>
+            <div class="icon-item" title="近卫"></div>
+            <div class="icon-item" title="辅助"></div>
+            <div class="icon-item" title="重装"></div>
+          </div>
+        </div>
+      </div>
+      <!-- 列表区 -->
+      <div class="list-area">
+        <div class="grid">
+          <!-- 使用v-for来遍历，根据星级的不同设置底部颜色 -->
+          <a class="card">
+            <img src="" alt="">
+            <!-- 角色名称 -->
+            <div class="card-name"></div>
+            <!-- 底部颜色 -->
+            <div class="card-bottom-color"></div>
+          </a>
+        </div>
       </div>
     </div>
   </div>
+
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { getAllcharacters, getCharacterRanks } from '@/api/character';
 
-const roles = ref([]);
-onMounted(async () => {
-  const res = await getAllcharacters();
-  roles.value = res.data;
-
-  // 请求每个角色的属性定位
-  await Promise.all(
-    roles.value.map(async (role) => {
-      const rankRes = await getCharacterRanks(role.id);
-      // console.log(rankRes.data[0].damage_type);
-
-      role.rankInfo = rankRes.data;
-    })
-  )
-})
-
-const getCharacterImg = (spell) => {
-  // console.log(spell);
-
-  return `http://localhost:3000/images/characters/${spell}/${spell}.png`
-}
-
-const getRankImg = (name) => {
-  console.log(name);
-  return `http://localhost:3000/images/general/definition/${name}`
-}
-
-const getDamageImg = (name) => {
-  console.log(name);
-  return `http://localhost:3000/images/general/type/${name}`
-}
 </script>
 
 <style scoped>
-#home {
-  display: flex;
-  min-height: 100vh;
-}
-
-/* 左侧导航栏 */
-.sidebar {
-  width: 200px;
-  background-color: #f4f4f4;
-  padding: 20px;
-}
-
-.sidebar ul {
-  list-style: none;
-  padding: 0;
-}
-
-.sidebar li {
-  padding: 10px;
-  cursor: pointer;
-}
-
-.sidebar li.active {
-  background-color: #ddd;
-}
-
-/* 主内容区 */
-.content {
-  flex: 1;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-}
-
-.secondary-nav {
-  background-color: #f0f0f0;
-  padding: 10px;
-}
-
-.secondary-nav ul {
-  list-style: none;
-  padding: 0;
+/* 外层盒子 */
+.outer-box{
   display: flex;
 }
 
-.secondary-nav li {
-  margin-right: 20px;
-  cursor: pointer;
+
+/* 左侧导航区 */
+.left-nav {
+  width: 8rem;
+  height: 100vh;
+  background-color: aqua;
 }
 
-/* 角色展示区域 */
-.role-display {
-  margin-top: 20px;
+.logo {
+  height: 6rem;
+  width: 8rem;
+  background-color: antiquewhite;
+  margin-bottom: 2rem;
 }
 
-.roles {
-  display: flex;
-  flex-wrap: wrap;
+.nav {
+  width: 8rem;
+  background-color: aquamarine;
 }
 
-.role {
-  width: 150px;
-  padding: 10px;
-  background-color: #f9f9f9;
-  margin-right: 10px;
-  margin-bottom: 10px;
+.nav-item {
+  height: 4rem;
+  /* 文字居中 */
   text-align: center;
+  line-height: 4rem;
+  font-size: 1.6rem;
 }
 
-.role p {
-  margin: 0;
+
+/* 右侧内容区 */
+.right-content{
+  height: 100%;
 }
 </style>
