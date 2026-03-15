7320dd65c44f6fdce24c3796f84dae8362a4afd6
Dayazs <Dayazs@outlook.com>
2026-03-15 17:54:43 +0800

    更改了访问路径

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,38 @@
 import { createRouter, createWebHistory } from "vue-router"
-import TheEndland from "@/view/TheEndland.vue"
-import TEcharacterDetail from "@/view/TEcharacterDetail.vue"
+import TheEndland from "@/view/endland/TheEndland.vue"
+import TEcharacterDetail from "@/view/endland/TEcharacterDetail.vue"
+import Home from "@/view/Home.vue"
+import Wuwa from "@/view/wuwa/Wuwa.vue"
+import WuwaDetail from "@/view/wuwa/WuwaDetail.vue"
 
 const routes = [
   {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+
+  // 鸣潮页面
+  {
+    path: '/wuwa',
+    name: 'Wuwa',
+    component: Wuwa
+  },
+  // 鸣潮角色详细页面
+  {
+    path: '/wuwa/wuwa_detail',
+    name: 'WuwaDetail',
+    component: WuwaDetail
+  },
+
+
+  // 终末地页面
+  {
     path: '/the_endland',
-    name: 'the_endland_home',
+    name: 'TheEndland',
     component: TheEndland
   },
+  // 终末地角色详细页面
   {
     path: '/the_endland/te_character_detail',
     name: 'TEcharacterDetail',
