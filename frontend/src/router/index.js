0939c0d9ec3409c6f060606ba299abe2b7cfbd52
Dayazs <Dayazs@outlook.com>
2026-03-25 22:32:17 +0800

    修改文件位置

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,36 +1,37 @@
-import { createRouter, createWebHistory } from "vue-router"
-import TheEndland from "@/view/endland/TheEndland.vue"
-import TEcharacterDetail from "@/view/endland/TEcharacterDetail.vue"
-import Home from "@/view/Home.vue"
-import Wuwa from "@/view/wuwa/Wuwa.vue"
-import WuwaDetail from "@/view/wuwa/WuwaDetail.vue"
+import { createRouter, createWebHistory } from 'vue-router'
+import TheEndland from '@/view/endland/TheEndland.vue'
+import TEcharacterDetail from '@/view/endland/TEcharacterDetail.vue'
+import Home from '@/view/Home.vue'
+import Wuwa from '@/view/wuwa/Wuwa.vue'
+import WuwaDetail from '@/view/wuwa/WuwaDetail.vue'
+
+import LoginRegister from '@/components/endland/LoginRegister.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
   },
 
   // 鸣潮页面
   {
     path: '/wuwa',
     name: 'Wuwa',
-    component: Wuwa
+    component: Wuwa,
   },
   // 鸣潮角色详细页面
   {
     path: '/wuwa/wuwa_detail',
     name: 'WuwaDetail',
-    component: WuwaDetail
+    component: WuwaDetail,
   },
-
 
   // 终末地页面
   {
     path: '/the_endland',
     name: 'TheEndland',
-    component: TheEndland
+    component: TheEndland,
   },
   // 终末地角色详细页面
   {
@@ -38,11 +39,11 @@ const routes = [
     name: 'TEcharacterDetail',
     component: TEcharacterDetail,
     // props: true
-  }
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 export default router
