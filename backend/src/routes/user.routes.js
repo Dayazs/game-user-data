a91d26c6b40fdc87705ad8970059eaae20e64f8c
Dayazs <Dayazs@outlook.com>
2026-03-30 16:03:18 +0800

    新增获取用户信息路由

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,10 @@
 import express from 'express'
-import { userRegister, userLogin } from '../controllers/user.controller.js'
+import {
+  userRegister,
+  userLogin,
+  userData,
+} from '../controllers/user.controller.js'
+import { auth } from '../middlewares/jwt/auth.js'
 
 const userRouter = express.Router()
 
@@ -8,5 +13,8 @@ userRouter.post('/register', userRegister)
 
 // 用户登录接口
 userRouter.post('/login', userLogin)
+
+// 获取用户信息接口
+userRouter.post('/userData', auth, userData)
 
 export default userRouter
