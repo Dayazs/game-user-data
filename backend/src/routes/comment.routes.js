ec108e2f9104b11eb7e321349802783a3c2ae6a4
Dayazs <Dayazs@outlook.com>
2026-03-30 16:02:50 +0800

    新增查找评论路由

--- backend/src/routes/comment.routes.js
+++ backend/src/routes/comment.routes.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import { postComment, likeComment } from '../controllers/comment.controller.js'
+import { postComment, likeComment,getCharacterIdByComment } from '../controllers/comment.controller.js'
 import { auth } from '../middlewares/jwt/auth.js'
 
 const commentRouter = express.Router()
 
+// 添加评论
 commentRouter.post('/addComment', auth, postComment)
+// 点赞/取消点赞评论
 commentRouter.post('/likeComment', auth, likeComment)
+// 获取相关评论
+commentRouter.get('/getComment', getCharacterIdByComment)
 
 export default commentRouter
