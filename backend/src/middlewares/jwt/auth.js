832a38da39020eb211119beb1e03409d4b51f983
Dayazs <Dayazs@outlook.com>
2026-03-30 16:01:22 +0800

    新增token验证提示

--- backend/src/middlewares/jwt/auth.js
+++ backend/src/middlewares/jwt/auth.js
@@ -6,15 +6,17 @@ const SECRET = secretData.SECRET
 function auth(req, res, next) {
   const token = req.headers.authorization?.split(' ')[1]
   if (!token) {
-    return res.json({ message: '未登录' })
+    console.log('token验证失败')
+    return res.json({ message: '未登录', code: 401 })
   }
 
   try {
     const decoded = jwt.verify(token, SECRET)
     req.user = decoded
+    console.log('token验证成功')
     next()
   } catch (err) {
-    res.json({ message: 'token无效' })
+    res.json({ message: 'token无效', code: 401 })
   }
 }
 
