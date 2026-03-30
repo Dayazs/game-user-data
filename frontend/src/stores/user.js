982119f0836e0026d9f5292bc2bf7d2e975f7502
Dayazs <Dayazs@outlook.com>
2026-03-30 17:07:39 +0800

    pinia存储文件

---
+++ frontend/src/stores/user.js
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    account_number: '',
+    user_id: null,
+    is_admin: '',
+    username: '',
+    user_avatar: '',
+    needRefreshComment: false,
+  }),
+
+  actions: {
+    setUser(user) {
+      this.account_number = user.account_number
+      this.user_id = user.id
+      this.is_admin = user.is_admin
+      this.username = user.username
+      this.user_avatar = user.user_avatar
+    },
+    triggerRefresh() {
+      this.needRefreshComment = !this.needRefreshComment
+    },
+  },
+})
