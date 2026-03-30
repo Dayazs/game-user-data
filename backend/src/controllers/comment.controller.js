fdd407e8b2216b5664a62186c2355a47a0e96951
Dayazs <Dayazs@outlook.com>
2026-03-30 15:59:42 +0800

    对接口返回数据进行修改，新增获取指定角色评论

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -1,14 +1,18 @@
-import { createComment, toggleLike } from '../services/comment.service.js'
+import {
+  createComment,
+  toggleLike,
+  findCharacterIdByComment,
+} from '../services/comment.service.js'
 
 // 发评论
 async function postComment(req, res) {
   try {
     const user_id = req.user.id
-    const { comment_text } = req.body
+    const { comment_text, character_id } = req.body
 
-    await createComment(user_id, comment_text)
+    await createComment(user_id, comment_text, character_id)
 
-    res.json({ message: '评论成功' })
+    res.json({ message: '评论成功' ,code:20000})
   } catch (err) {
     res.json({ message: err.message })
   }
@@ -18,13 +22,29 @@ async function postComment(req, res) {
 async function likeComment(req, res) {
   try {
     const user_id = req.user.id
-    const { comment_id } = req.body
+    const { comment_id, character_id } = req.body
 
-    const msg = await toggleLike(user_id, comment_id)
+    const msg = await toggleLike(user_id, comment_id, character_id)
     res.json({ message: msg })
   } catch (err) {
     res.json({ message: err.message })
   }
 }
 
-export { postComment, likeComment }
+// 获取某位角色评论
+async function getCharacterIdByComment(req, res) {
+  try {
+    const { user_id, character_id } = req.query
+    const commentArr = await findCharacterIdByComment(+user_id, +character_id)
+    // console.log(commentArr)
+    res.json({ message: '评论数据获取成功', commentArr })
+  } catch (err) {
+    res.json({ message: err.message })
+  }
+}
+
+export { postComment, likeComment, getCharacterIdByComment }
+
+// 销毁前解绑
+// 计算属性中使用异步
+// mounted使用async对其他函数有影响吗
